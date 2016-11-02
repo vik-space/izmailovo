1730f4bb4d73883d3b28d1444571ea47975016e9
Vasil Korkin <v.ivan.korkin@gmail.com>
2016-11-02 15:35:54 +0400

    Черновая верстка страницы со списком банкетных залов

--- app/js/common.js
+++ app/js/common.js
@@ -169,6 +169,23 @@ $(function () {
 
     }
 
+    function setEqualHeight(columns)
+    {
+        var tallestcolumn = 0;
+        columns.each(
+            function()
+            {
+                currentHeight = $(this).height();
+                if(currentHeight > tallestcolumn)
+                {
+                    tallestcolumn = currentHeight;
+                }
+            }
+        );
+        columns.height(tallestcolumn);
+    }
+
+    setEqualHeight($(".banqueting_hall  .hotel_room"));
 
     $(".date").kendoDatePicker({
         value: new Date(),
